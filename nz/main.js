135c3bb02c6187fd89cee7c84500b71938e9a182
Lucas Weinberg <Lucas.Weinberg@uibk.com>
2022-03-31 17:22:35 +0200

    nach main js verschoben

---
+++ nz/main.js
@@ -0,0 +1,10 @@
+//
+var map = L.map('map').setView([-44.616667, 167.866667], 11);
+                  
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([-44.616667,167.866667 ]).addTo(map)
+.bindPopup('<h3>Tongariro Nationalpark </h3>.')
+.openPopup();
