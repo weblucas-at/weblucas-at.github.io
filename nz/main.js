1fa72b565b868bccd272feb7e199a0cae77beb4d
Lucas Weinberg <Lucas.Weinberg@uibk.com>
2022-04-07 16:14:37 +0200

    popup als Variable mit Titel, Lat und Lng

--- nz/main.js
+++ nz/main.js
@@ -3,8 +3,15 @@
 let lat = -44.616667;
 let lng = 167.866667;
 let zoom = 11;
+
+  let coords = [ -39.13, 175.65];
+console.log (coords);   
+console.log(coords [0]);
+console.log(coords [1]);
+console.log(coords.length);
+
 let map = L.map('map').setView([lat, lng], zoom);
-                  
+
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
