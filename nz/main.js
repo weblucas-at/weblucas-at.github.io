d0b7d6298bcb6d0e4f7bdf74b5ce287a1c86f391
Lucas Weinberg <Lucas.Weinberg@uibk.com>
2022-04-07 15:43:54 +0200

    Variable let

--- nz/main.js
+++ nz/main.js
@@ -1,11 +1,14 @@
 /*Neuseelandreise Skript*/
 //einzelner Kommentar
-var map = L.map('map').setView([-44.616667, 167.866667], 11);
+let lat = -44.616667;
+let lng = 167.866667;
+let zoom = 11;
+let map = L.map('map').setView([lat, lng], zoom);
                   
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([-44.616667,167.866667 ]).addTo(map)
+L.marker([lat,lng ]).addTo(map)
 .bindPopup('<h3>Milford Sound </h3>.')
 .openPopup();
