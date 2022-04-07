d0c00149fa76a21be2f14ee4e3884a4335ebf7c2
Lucas Weinberg <Lucas.Weinberg@uibk.com>
2022-04-07 16:48:43 +0200

    Etappenobjekt beim Ersten

--- nz/main.js
+++ nz/main.js
@@ -9,12 +9,23 @@ console.log (coords);
 console.log(coords [0]);
 console.log(coords [1]);
 console.log(coords.length);
-let popup = `<h3>Milford Sound </h3>
+let popup = `<h3>${ETAPPEN[0].titel} </h3>
 <ul>
-<li>geogr. Länge: ${lng}</li>
-<li>geogr. Breite: ${lat} </li>
+<li>geogr. Länge: ${ETAPPEN[0].lng}</li>
+<li>geogr. Breite: ${ETAPPEN[0].lat} </li>
+<li><a href="${ETAPPEN[0].wikipedia}">Link zur Wikipediaseite</a></li>
+<li><a href="${ETAPPEN[0].github}">Link zur Etappenseite</a></li>
 </ul>
 `
+
+console.log (ETAPPEN);
+console.log (ETAPPEN[0]);
+console.log (ETAPPEN[0].nr);
+console.log (ETAPPEN[0].github);
+console.log (ETAPPEN[0].titel);
+console.log (ETAPPEN[0].wikipedia);
+console.log (ETAPPEN[0].lat);
+console.log (ETAPPEN[0].lng);
 
 console.log("text");
 console.log('id="map"');
