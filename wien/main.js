a577aa40a3616fe75d411f7c3ae0d96947c5d99a
Lucas Weinberg <Lucas.Weinberg@uibk.com>
2022-05-12 15:45:26 +0200

    Liniennetz

--- wien/main.js
+++ wien/main.js
@@ -143,7 +143,17 @@ loadStops("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&vers
      overlay.addTo(map);
  
  
-     L.geoJSON(geojson).addTo(overlay);
+     L.geoJSON(geojson).bindPopup(function (layer) {
+        return`
+        <h4>${layer.feature.properties.LINE_NAME}</h4>
+        von: ${layer.feature.properties.FROM_NAME}
+        <br> 
+        nach: ${layer.feature.properties.TO_NAME}
+        
+        `
+
+        //return layer.feature.properties.LINE_NAME;
+    })addTo(overlay);
 }
 loadLines("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKLINIEVSLOGD&srsName=EPSG:4326&outputFormat=json");
 
